d11ae56f24db0a811e372fa71f95e2eed10a7f04
rbengtsson <robertbengtsson229@gmail.com>
2016-11-15 14:31:05 +0100

    * Improved nuget package * Decreased Moq dependency version

--- Automoqer/Properties/AssemblyInfo.cs
+++ Automoqer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Automoqer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automatically creates services for unit testing with all constructor parameters as Moq-objects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Robert Bengtsson")]
 [assembly: AssemblyProduct("Automoqer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Robert Bengtsson 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
