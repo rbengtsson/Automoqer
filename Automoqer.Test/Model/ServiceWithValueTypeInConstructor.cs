d53d345d762fb158b7b01032dd5bd8014c7b0e1c
rbengtsson <robertbengtsson229@gmail.com>
2016-11-23 15:33:48 +0100

    * Controlled failing when service has value type in constructor * Added unit test to handle value types in constructor

---
+++ Automoqer.Test/Model/ServiceWithValueTypeInConstructor.cs
@@ -0,0 +1,9 @@
+﻿namespace Automoqer.Test.Model
+{
+    public class ServiceWithValueTypeInConstructor
+    {
+        public ServiceWithValueTypeInConstructor(int intVal)
+        {            
+        }
+    }
+}
