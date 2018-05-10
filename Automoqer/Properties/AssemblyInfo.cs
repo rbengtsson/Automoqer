d25cc75d650b69d11d8d0e429c8fead37f8820bd
manne <manuel.pfemeter@gmx.de>
2018-05-10 19:19:31 +0200

    migrates to new project system

--- Automoqer/Properties/AssemblyInfo.cs
+++ Automoqer/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Automoqer")]
-[assembly: AssemblyDescription("Automatically creates services for unit testing with constructor parameters as Moq-objects. Docs and info are available on the project's github site https://github.com/rbengtsson/Automoqer")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Robert Bengtsson")]
-[assembly: AssemblyProduct("Automoqer")]
-[assembly: AssemblyCopyright("Copyright © Robert Bengtsson 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e642c604-bcad-4659-9858-dc68f525af77")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
