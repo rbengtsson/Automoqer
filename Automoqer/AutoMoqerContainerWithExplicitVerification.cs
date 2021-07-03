3a43aca1e4b00cbfab79f871850117b9d0fef425
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2021-07-03 17:23:00 +0200

    Merge branch 'upstream/master'
    # Conflicts:
    #	Automoqer.Test/AutoMoqerTest.cs
    #	Automoqer/Properties/AssemblyInfo.cs

--- Automoqer/AutoMoqerContainerWithExplicitVerification.cs
+++ Automoqer/AutoMoqerContainerWithExplicitVerification.cs
@@ -1,17 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 namespace Automoqer
 {
     /// <summary>
-    /// A container for AutoMoqer that holds all constructor dependencies for a service
+    /// A container for AutoMoqer that holds all constructor dependencies for a service and allows verifying all expectations.
     /// </summary>
     /// <typeparam name="TService">Type of the service to mock dependencies for</typeparam>
-    public class AutoMoqerContainer<TService> : AutoMoqerContainerBase<TService>, IDisposable
+    public class AutoMoqerContainerWithExplicitVerification<TService> : AutoMoqerContainerBase<TService>
     {
-        internal AutoMoqerContainer(
+        protected internal AutoMoqerContainerWithExplicitVerification(
             ConstructorInfo primaryConstructor,
             IReadOnlyDictionary<Type, object> exceptionParametersByType,
             IReadOnlyDictionary<string, object> exceptionParametersByName)
@@ -21,14 +20,10 @@ namespace Automoqer
         }
 
         /// <summary>
-        /// Will run VerifyAll on all Moq-parameters
+        /// Will run <see cref="Moq.Mock.VerifyAll"/> on all Moq-parameters.
         /// </summary>
-        public void Dispose()
+        public void VerifyAll()
         {
-            var exceptionOccurred = Marshal.GetExceptionPointers() != IntPtr.Zero || Marshal.GetExceptionCode() != 0;
-            if (exceptionOccurred)
-                return;
-
             VerifyAllInstances();
         }
     }
